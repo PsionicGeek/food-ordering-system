ad9f732bf0d932f1233bb7efc4a45c323780fcca
mittalgarima <garimamittal101@gmail.com>
2024-01-09 18:17:40 +0530

    created header, slider and categories

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
-
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Link,
-    createBrowserRouter,
-    createRoutesFromElements, RouterProvider
-} from 'react-router-dom';
-import AdminDashboard from "./Admin/admin";
-import SideNavBar from "./Admin/SideNavBar";
-import AdminHome from "./Admin/components/AdminHome";
-import Categories from "./Admin/components/Categories";
-import Dishes from "./Admin/components/Dishes";
-import Orders from "./Admin/components/Orders";
-import Users from "./Admin/components/Users";
-import Profile from "./Admin/components/Profile";
-import AdminLogin from "./Admin/AdminLogin";
-
-
-const router= createBrowserRouter(
-    createRoutesFromElements(
-        <Route>
-
-            <Route path="/admin-login" element={<AdminLogin />} />
-
-        <Route path="/admin" element={<AdminHome />}>
-
-            <Route path="categories"  element={<Categories/>} />
-            <Route path="dishes"  element={<Dishes/>} />
-            <Route path="orders"  element={<Orders/>} />
-            <Route path="users"  element={<Users/>} />
-            <Route path="profile"  element={<Profile/>} />
-        </Route>
-        </Route>
-
-    )
-)
+import React from "react";
+import CustomerHome from "./component/customer/Home/CustomerHome";
+import {BrowserRouter,Routes, Route} from "react-router-dom";
+import './App.css';
+import Profile from "./component/customer/Profile/Profile";
+import Cart from "./component/customer/cart/cart";
+import Login from './component/customer/Login/login'
+import store from './redux/store';
+import { getTotals } from "./component/customer/cart/cartSlice";
+import { Provider } from "react-redux";
+import Alldish from './component/customer/Alldish/alldish';
 function App() {
+  store.dispatch(getTotals());
+
   return (
+    <BrowserRouter>
+    <Provider store={store}>
+    <Routes>
+    <Route path='/' element={<CustomerHome />} />
+    <Route path='/cart' element={<Cart />} />
+    <Route path='/profile' element={<Profile />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/alldish' element={<Alldish />}/>
+    </Routes>
+    </Provider>
+  </BrowserRouter>
+  
+);
 
-      <div>
-
-          <RouterProvider router={router}/>
-      </div>
-
-  );
+       
 }
 
 export default App;
