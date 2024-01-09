f94cf642fdfe0b7802c09c33b06047ec453165ca
PsionicGeek <rrkumar220920@gmail.com>
2024-01-09 23:02:50 +0530

    Added basic Skeleton Structure of the website. Will add proper styling later

---
+++ src/Admin/components/Categories.js
@@ -0,0 +1,219 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import {alpha, Box, Button, Container, InputBase, Stack, SvgIcon, Typography} from '@mui/material';
+import {ArrowDownOnSquareIcon, ArrowUpOnSquareIcon, PlusIcon} from "@heroicons/react/20/solid";
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import SearchIcon from "@mui/icons-material/Search";
+import {styled} from "@mui/material/styles";
+
+const categories=[
+    {
+        id: 1,
+        name: "Category 1",
+        description: "Category 1 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 2,
+        name: "Category 2",
+        description: "Category 2 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 3,
+        name: "Category 3",
+        description: "Category 3 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 4,
+        name: "Category 4",
+        description: "Category 4 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 5,
+        name: "Category 5",
+        description: "Category 5 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 6,
+        name: "Category 6",
+        description: "Category 6 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    },
+    {
+        id: 7,
+        name: "Category 7",
+        description: "Category 7 description",
+        image: "https://media.istockphoto.com/id/146807105/photo/food-pyramid-pie-chart.jpg?s=612x612&w=0&k=20&c=SX0hFBaED3Wwi0G2pLfhsYN1GRjlyK8wzqHf-qUyJOk=",
+        created_at: "2016-11-22T15:28:52.000Z",
+        updated_at: "2016-11-22T15:28:52.000Z",
+        deleted_at: null
+    }
+];
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+export default function Categories() {
+    return (
+        <div>
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    py: 8
+                }}
+            >
+                <Container maxWidth="xl">
+                    <Stack spacing={3}>
+                <Stack
+                direction="row"
+                justifyContent="space-between"
+                spacing={4}
+            >
+                <Stack spacing={1} >
+                    <Typography variant="h4" paddingLeft={15}>
+                        Categories
+                    </Typography>
+
+                </Stack>
+                <div>
+                    <Stack paddingRight={15} direction={'row'}>
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                    <Button
+
+                        startIcon={(
+                            <SvgIcon fontSize="small" >
+                                <PlusIcon />
+                            </SvgIcon>
+                        )}
+                        variant="contained"
+                    >
+                        Add
+                    </Button>
+                    </Stack>
+                </div>
+            </Stack>
+        <div style={{justifyContent:'center', display:'flex'}}>
+
+        <div style={{ height: 600, width: 1200 }} >
+
+            <MDBTable align='middle'>
+            <MDBTableHead>
+            <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Description</th>
+            <th scope='col'>Id</th>
+            <th scope='col'>Actions</th>
+
+            </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+
+{categories.map((category) => (
+
+            <tr>
+            <td>
+            <div className='d-flex align-items-center'>
+            <img
+            src={category.image}
+            alt=''
+            style={{ width: '45px', height: '45px' }}
+            className='rounded-circle'
+        />
+            <div className='ms-3'>
+                <p className='fw-bold mb-1'>{category.name}</p>
+
+            </div>
+        </div>
+                    </td>
+                    <td>
+                        <p className='fw-normal mb-1'>{category.description}</p>
+
+                    </td>
+
+                    <td>{category.id}</td>
+                    <td>
+                        <MDBBtn color='link' rounded size='sm'>
+                            Edit
+                        </MDBBtn>
+                    </td>
+                </tr>
+    ))}
+
+            </MDBTableBody>
+        </MDBTable>
+
+        </div>
+        </div>
+                    </Stack>
+                </Container>
+            </Box>
+        </div>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Link,
+    createBrowserRouter,
+    createRoutesFromElements, RouterProvider
+} from 'react-router-dom';
+import AdminDashboard from "./Admin/admin";
+import SideNavBar from "./Admin/SideNavBar";
+import AdminHome from "./Admin/components/AdminHome";
+import Categories from "./Admin/components/Categories";
+import Dishes from "./Admin/components/Dishes";
+import Orders from "./Admin/components/Orders";
+import Users from "./Admin/components/Users";
+import Profile from "./Admin/components/Profile";
+import AdminLogin from "./Admin/AdminLogin";
+
+
+const router= createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+
+            <Route path="/admin-login" element={<AdminLogin />} />
+
+        <Route path="/admin" element={<AdminHome />}>
+
+            <Route path="categories"  element={<Categories/>} />
+            <Route path="dishes"  element={<Dishes/>} />
+            <Route path="orders"  element={<Orders/>} />
+            <Route path="users"  element={<Users/>} />
+            <Route path="profile"  element={<Profile/>} />
+        </Route>
+        </Route>
+
+    )
+)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div>
+
+          <RouterProvider router={router}/>
+      </div>
+
   );
 }
 
