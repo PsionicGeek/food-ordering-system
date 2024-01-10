176f71b09810ae244ebe71ee2759533377edeab6
PsionicGeek <rrkumar220920@gmail.com>
2024-01-10 11:30:51 +0530

    Merged Branches

--- src/App.js
+++ src/App.js
@@ -1,51 +1,73 @@
-
-import React from 'react';
+import React from "react";
+import CustomerHome from "./component/customer/Home/CustomerHome";
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Link,
-    createBrowserRouter,
-    createRoutesFromElements, RouterProvider
-} from 'react-router-dom';
-import AdminDashboard from "./Admin/admin";
-import SideNavBar from "./Admin/SideNavBar";
-import AdminHome from "./Admin/components/AdminHome";
-import Categories from "./Admin/components/Categories";
-import Dishes from "./Admin/components/Dishes";
-import Orders from "./Admin/components/Orders";
-import Users from "./Admin/components/Users";
-import Profile from "./Admin/components/Profile";
-import AdminLogin from "./Admin/AdminLogin";
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+import './App.css';
+import Profile from "./component/customer/Profile/Profile";
+import Cart from "./component/customer/cart/cart";
+import Login from './component/customer/Login/login'
+import store from './redux/store';
+import { getTotals } from "./component/customer/cart/cartSlice";
+import { Provider } from "react-redux";
+import Alldish from './component/customer/Alldish/alldish';
+import Menu from "./component/customer/Menu/menu";
+import AdminLogin from "./component/Admin/AdminLogin";
+import AdminHome from "./component/Admin/components/AdminHome";
+import Categories from "./component/Admin/components/Categories";
+import Dishes from "./component/Admin/components/Dishes";
+import Orders from "./component/Admin/components/Orders";
+import Users from "./component/Admin/components/Users";
+import AdminProfile from "./component/Admin/components/Profile";
+
+
+
+
 
 
 const router= createBrowserRouter(
     createRoutesFromElements(
         <Route>
 
-            <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path='/' element={<CustomerHome />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/alldish' element={<Alldish />}/>
+          <Route path='/menu' element={<Menu />}/>
+          <Route path="/admin-login" element={<AdminLogin />} />
 
-        <Route path="/admin" element={<AdminHome />}>
+          <Route path="/admin" element={<AdminHome />}>
 
             <Route path="categories"  element={<Categories/>} />
             <Route path="dishes"  element={<Dishes/>} />
             <Route path="orders"  element={<Orders/>} />
             <Route path="users"  element={<Users/>} />
-            <Route path="profile"  element={<Profile/>} />
-        </Route>
+            <Route path="profile"  element={<AdminProfile/>} />
+          </Route>
         </Route>
 
     )
 )
 function App() {
+  // store.dispatch(getTotals());
   return (
 
       <div>
-
-          <RouterProvider router={router}/>
+        <Provider store={store}>
+        <RouterProvider router={router}/>
+          </Provider>
       </div>
 
   );
 }
 
 export default App;
+
+
+
