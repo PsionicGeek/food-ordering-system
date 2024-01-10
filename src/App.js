68f299c83e65b2d960b146b4c9b6773257c3e128
PsionicGeek <rrkumar220920@gmail.com>
2024-01-10 10:52:15 +0530

    Updated router from BrowserRouter to createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import CustomerHome from "./component/customer/Home/CustomerHome";
-import {BrowserRouter,Routes, Route} from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
 import './App.css';
 import Profile from "./component/customer/Profile/Profile";
 import Cart from "./component/customer/cart/cart";
@@ -10,27 +17,73 @@ import { getTotals } from "./component/customer/cart/cartSlice";
 import { Provider } from "react-redux";
 import Alldish from './component/customer/Alldish/alldish';
 import Menu from "./component/customer/Menu/menu";
-function App() {
-  store.dispatch(getTotals());
 
+
+
+
+
+
+const router= createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+
+          <Route path='/' element={<CustomerHome />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/alldish' element={<Alldish />}/>
+          <Route path='/menu' element={<Menu />}/>
+          {/*<Route path="/admin-login" element={<AdminLogin />} />*/}
+
+          {/*<Route path="/admin" element={<AdminHome />}>*/}
+
+          {/*  <Route path="categories"  element={<Categories/>} />*/}
+          {/*  <Route path="dishes"  element={<Dishes/>} />*/}
+          {/*  <Route path="orders"  element={<Orders/>} />*/}
+          {/*  <Route path="users"  element={<Users/>} />*/}
+          {/*  <Route path="profile"  element={<Profile/>} />*/}
+          {/*</Route>*/}
+        </Route>
+
+    )
+)
+function App() {
+  // store.dispatch(getTotals());
   return (
 
-    <BrowserRouter>
-    <Provider store={store}>
-    <Routes>
-    <Route path='/' element={<CustomerHome />} />
-    <Route path='/cart' element={<Cart />} />
-    <Route path='/profile' element={<Profile />} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/alldish' element={<Alldish />}/>
-    <Route path='/menu' element={<Menu />}/>
-    </Routes>
-    </Provider>
-  </BrowserRouter>
+      <div>
+        <Provider store={store}>
+        <RouterProvider router={router}/>
+          </Provider>
+      </div>
 
-);
-
-
+  );
 }
 
 export default App;
+// function App() {
+//   store.dispatch(getTotals());
+//
+//   return (
+//
+//     <BrowserRouter>
+//     <Provider store={store}>
+//     <Routes>
+//     <Route path='/' element={<CustomerHome />} />
+//     <Route path='/cart' element={<Cart />} />
+//     <Route path='/profile' element={<Profile />} />
+//     <Route path='/login' element={<Login />} />
+//     <Route path='/alldish' element={<Alldish />}/>
+//     <Route path='/menu' element={<Menu />}/>
+//     </Routes>
+//     </Provider>
+//   </BrowserRouter>
+//
+// );
+//
+//
+// }
+//
+// export default App;
+
+
